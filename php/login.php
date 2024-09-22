13ffd3ad2f616f5ab4e6d364f7a6fb0cf8f9e68a
iliyashev <iliya.shevolaev@gmail.com>
2024-09-22 16:55:34 +0300

    data security commit

--- php/login.php
+++ php/login.php
@@ -3,24 +3,29 @@
 include "dbcon.php";
 
 $loginUser = [
-    "user_login" => $_POST["username"],
-    "user_password" => $_POST["password"]
+    "user_login" => htmlspecialchars($_POST["username"]),
+    "user_password" => htmlspecialchars($_POST["password"])
 ];
 
 $stringConnection = "host=localhost port=5432 dbname=registerdb user=postgres password=12345";
 $newConnection = new DbManager($stringConnection);
 
 if ($newConnection->connectTo()) {
-    $query = "SELECT * FROM userdata WHERE user_login = '" . $loginUser["user_login"] . "' AND user_password = '" . $loginUser["user_password"] . "' ";
+    $query = "SELECT * FROM userdata WHERE user_login = '" . $loginUser["user_login"] . "' ";
     
     $resultArray = $newConnection->makeQuery($query);
 
     if (!empty($resultArray)) {
-        header("Location: /html/succsess.html");
-        exit;
-    } else {
-        echo "Ошибка входа";
-    }
+
+        $hashPass = $resultArray["user_password"];
+        if (password_verify($loginUser["user_password"], $hashPass)) {
+            $newConnection->closeConnection();
+            header("Location: /html/succsess.html");
+            exit;
+        }
+
+    } 
 }
 
+echo "Ошибка входа";
 $newConnection->closeConnection();
--- php/register.php
+++ php/register.php
@@ -3,8 +3,8 @@
 include "dbcon.php";
 
 $newUserArray = [
-    "user_login" => $_POST["username"],
-    "user_password" => $_POST["password"]
+    "user_login" => htmlspecialchars($_POST["username"]),
+    "user_password" => password_hash($_POST["password"], PASSWORD_DEFAULT)
 ];
 
 $stringConnection = "host=localhost port=5432 dbname=registerdb user=postgres password=12345";
